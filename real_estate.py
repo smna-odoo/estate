f34037aa235929ecadf53c52797f9d4f24d8f71d
smnaa <smna@odoo.com>
2022-02-03 23:28:35 +0530

    comment

--- real_estate.py
+++ real_estate.py
@@ -16,7 +16,7 @@ uid = common.authenticate(db, username, password, {})
 if uid:
     print ("Connection Successful")
 
-models = xmlrpc.client.ServerProxy('http://localhost:8069/xmlrpc/2/object')
+# models = xmlrpc.client.ServerProxy('http://localhost:8069/xmlrpc/2/object')
 
 # models.execute_kw(db, uid, password, 'course.course', 'create', [{'name': 'Statistics', 'state': 'new'}])
 
